e53365f9ba4d0e302a09488e84c25a147c49f752
BenediktHoelker <benedikt.hoelker@gmail.com>
2016-12-14 10:01:08 +0100

    Enabled Material Design with hammer.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { DeliveryService } from './shared/delivery.service';
